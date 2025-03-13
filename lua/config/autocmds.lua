e118c2ecaa2cbc66392c0f5522dbce902fe9104a
VimHater <24520826@gm.uit.edu.vn>
2025-03-13 22:54:20 +0700

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,13 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.cmd("Copilot disable")
+vim.keymap.set({"n", "v" }, "<leader>cb", function()
+    if vim.opt.clipboard:get()[1] == "unnamedplus" then
+        vim.opt.clipboard = "unnamed"
+        print("Clipboard: unnamed (Primary Selection)")
+    else
+        vim.opt.clipboard = "unnamedplus"
+        print("Clipboard: unnamedplus (System Clipboard)")
+    end
+end, { desc = "Toggle clipboard between unnamed and unnamedplus" })
+
