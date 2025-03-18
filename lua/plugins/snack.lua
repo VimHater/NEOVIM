6729ee9b15cb0fd06358b222192fdcb0000dc3f3
VimHater <24520826@gm.uit.edu.vn>
2025-03-18 21:18:07 +0700

    update

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         dashboard = {
             preset = {
-                pick = function(cmd, opts) end,
+                --pick = function(cmd, opts) end,
                 header = [[
    ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗
    ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║
@@ -13,17 +13,17 @@ return {
    ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝
 ]],
         -- stylua: ignore
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
+        -- keys = {
+        --   { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+        --   { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+        --   { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+        --   { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+        --   { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+        --   { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+        --   { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+        --   { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+        --   { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        -- },
             },
         },
     },
