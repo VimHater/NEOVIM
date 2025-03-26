10d5217e29d0e7b92eb01b04da2c540954ef4c4a
VimHater <24520826@gm.uit.edu.vn>
2025-03-26 15:46:30 +0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -34,3 +34,21 @@ vim.opt.sidescrolloff = 12
 vim.opt.wrap = false
 --vim.opt.termguicolors = false
 -- enabled with `:LazyExtras`
+--vim.o.scrolloff = 999
+-- vim.api.nvim_create_autocmd("CursorMoved", {
+--   pattern = "*",
+--   command = "normal! zz",
+-- })
+
+if vim.g.neovide then
+    -- Set font
+    vim.o.guifont = "JetbrainsMono Nerd Font:s16"
+
+    vim.g.neovide_transparency = 1
+
+    vim.g.neovide_scale_factor = 1
+
+    --vim.g.neovide_cursor_vfx_mode = "railgun"
+
+    vim.g.neovide_scroll_animation_length = 0.3
+end
