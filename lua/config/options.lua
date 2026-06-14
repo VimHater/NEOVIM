beb2f6ddb78fc75a5585ba0f267fdc5c1fbf509e
VimHater <24520826@gm.uit.edu.vn>
2026-06-14 21:09:00 +0700

    udpate

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -113,6 +113,7 @@ vim.diagnostic.config({
 if vim.g.neovide then
     vim.keymap.set({'i', 'n'}, '<C-S-v>', '<C-r>+', { noremap = true, silent = true })
     vim.keymap.set('c', '<C-S-v>', '<C-r>+', { noremap = true })
+    vim.opt.guifont = "ZedMono Nerd Font Mono"
 
     vim.opt.linespace = 3
     vim.g.neovide_input_ime = true
